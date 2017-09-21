9be7762dbc0494e1641ae0308069924a0b0cc907
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-21 19:42:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,14 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+		 programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for]
-
-	programmer_hash =
+		programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -37,6 +34,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
