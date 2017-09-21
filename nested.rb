b8e49de08ea5369ccc72a9b57159181a9821523a
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-21 19:44:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,25 +15,25 @@ def hopper
           :languages => ["C"]
         }
      }
-		 programmer_hash[:grace_hopper]
+	programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 		programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	 		{
+	        :grace_hopper => {
+	          :known_for => "COBOL",
+	          :languages => ["COBOL", "FORTRAN"]
+	        },
+	        :alan_kay => {
+	          :known_for => "Object Orientation",
+	          :languages => ["Smalltalk", "LISP"]
+	        },
+	        :dennis_ritchie => {
+	          :known_for => "Unix",
+	          :languages => ["C"]
+	        }
+	     }
 		 programmer_hash[:alan_kay][:known_for]
 end
 
@@ -56,12 +56,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -77,7 +71,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
@@ -98,7 +92,7 @@ def changing_alan
         }
      }
 		 alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for]
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
